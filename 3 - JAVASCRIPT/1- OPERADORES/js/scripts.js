fdba7f00211735b4912e19b13229e5fbf050d7ed
Alexadre-lima <alexandrr.ztein7@gmail.com>
2026-06-12 10:42:05 -0300

    Update scripts.js

--- 3 - JAVASCRIPT/1- OPERADORES/js/scripts.js
+++ 3 - JAVASCRIPT/1- OPERADORES/js/scripts.js
@@ -2,3 +2,34 @@
 console.log(typeof 2)
 console.log(typeof 1.52)
 console.log(typeof -5)
+
+// 2 - Operadores aritméticos
+console.log(4 + 5)
+console.log(4 - 5)
+console.log(4 * 5)
+console.log(4 / 5)
+
+// 3 - Special  Numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * "dois")
+console.log(typeof NaN)
+
+
+// 4 - strings
+console("Alexandre")
+console("Um texto")
+console("Bom dia")
+
+// 5 -  Mais sobre Strings
+console.log('Testando a \n quebra de linha')
+console.log("Espaçamento \t de tab")
+
+// 6 - Concatenação
+console.log("Oi, ", + "tudo bem?")
+
+// 7 - Interpolação(Template Strings)
+console.log(`Soma de 2 + 2 = {2 + 2}`)
+console.log(`Podemos executar qauqler coisa aqui${console.log("teste")}`)
+
+// 8 - Booleans
