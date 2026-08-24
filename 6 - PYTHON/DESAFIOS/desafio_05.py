de5a1f73841874b1b4b6e11a7f50c116a2e47935
Alexadre-lima <alexandrr.ztein7@gmail.com>
2026-08-23 21:47:55 -0300

    Create desafio_05.py

---
+++ 6 - PYTHON/DESAFIOS/desafio_05.py
@@ -0,0 +1,12 @@
+nome = "Alexandre"
+sobrenome = "Lima"
+idade = 30
+ano_nascimento = 2026 - idade
+maior_idade = idade >= 18
+altura_metro = 1.70
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Ano de nascimento: {ano_nascimento}")
+print(f"É maior de idade? {maior_idade}")
+print(f"Altura em metros: {altura_metro}")
