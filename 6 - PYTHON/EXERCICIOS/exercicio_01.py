23020dd74af7c886e98ef26c063fae08b5095f82
Alexadre-lima <alexandrr.ztein7@gmail.com>
2026-08-29 21:07:00 -0300

    voltando a praticar

---
+++ 6 - PYTHON/EXERCICIOS/exercicio_01.py
@@ -0,0 +1,12 @@
+'''
+Interpolação básca de strings
+s - strings
+d e i - int
+f - float
+x e X - HEXADECIMAL(ABCDEF0123456789)
+'''
+
+nome = 'Alexandre'
+preco = 1000.0985632
+variavel = '%s, preço é R$%.2f' %(nome, preco)
+print(variavel)
---
+++ 6 - PYTHON/EXERCICIOS/exercicio_03.py
@@ -0,0 +1,15 @@
+"""
+Fatiamneto de strings
+    0123456789
+    Olá munod
+    -9876543210
+Fatiamento [i:f:p] [::]
+Obs.: a fiunção de len a qtd
+de caracteres str
+"""
+variavel = "Ola mundo"
+print(variavel[5])
+print(variavel[-4])
+print(variavel[5:8])
+print(variavel[:5])
+print(len(variavel[0]))
