80cf6666133ee93c46c01e340c70f4160faa23b2
Alexandre Lima <139510050+Alexadre-lima@users.noreply.github.com>
2026-08-30 21:48:23 -0300

    Colocando a mão na massa com python

---
+++ 6 - PYTHON/EXERCICIOS/exercicio_04.py
@@ -0,0 +1,20 @@
+"""
+Introdução ao try/except
+ty -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+numero_str = input("Vou dobrar o número que vc digitar: ")
+
+try:
+    print(f"STR: {numero_str}")
+    numero_float =  float(numero_str)
+    print(f"FLOAT: {numero_float}")
+    print(f"O dobro de  {numero_str} é {numero_float * 2: }")
+except:
+    print("Isso não é um número!")
+
+# if numero_str.isdigit():
+#     numero_float =  float(numero_str)
+#     print(f"O dobro de  {numero_str} é {numero_float * 2: }")
+# else:
+#     print("Isso não é um número!")
---
+++ 6 - PYTHON/EXERCICIOS/exercicio_05.py
@@ -0,0 +1,25 @@
+'''
+CONSTANTE = "Variáveis" que não vaõ mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade(ruim)
+'''
+velocidade = 61 # velocidade atual do carro
+local_carro = 100 # local em o carro está na estrada
+
+RADA_1 = 60 # velocidade máxima do radar 1
+LOCAL_1 = 100 # local onde o radar 1 está 
+RADA_RANGE = 1 # A distancia onde o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADA_1
+carro_pass_radar_1 = LOCAL_1 - RADA_RANGE
+carro_multado_radar_1 = LOCAL_1 + RADA_RANGE
+
+if vel_carro_pass_radar_1:
+    print("Velocidade do carro passou do radar 1")
+
+if carro_pass_radar_1:
+    print("Carro passou no radar 1")
+
+if carro_multado_radar_1:
+    print("Carro multado em radar 1")
+    
