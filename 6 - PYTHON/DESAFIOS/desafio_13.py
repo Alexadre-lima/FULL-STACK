1aa63a82cfe7f0707a94c49d5bb0436a4e865678
Alexadre-lima <alexandrr.ztein7@gmail.com>
2026-09-25 23:46:49 -0300

    Voltando a pratica

--- 6 - PYTHON/DESAFIOS/desafio_13.py
+++ 6 - PYTHON/DESAFIOS/desafio_13.py
@@ -5,6 +5,8 @@ while True:
     operador = input("Qual operador matematico vai usar: ")
 
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
         num_1_float = float(numero_1)
@@ -23,8 +25,25 @@ while True:
         print('Operador inválido.')
         continue
 
-    if len
+    if len(operador) > 1:
+        print("Digite apenas um operador.")
+        continue
+
+    print("Realizando sua conta. Confira o resultado abaixo")
+    
+    if operador == "+":
+        print(f'{num_1_float} + {num_2_float} = ', num_1_float + num_2_float)
+    elif operador == "-":
+        print(f'{num_1_float} - {num_2_float} = ', num_1_float - num_2_float)
+    elif operador == "*":
+        print(f'{num_1_float} * {num_2_float} = ',num_1_float * num_2_float)
+    elif operador == "/":
+        print(f'{num_1_float} / {num_2_float} = ', num_1_float / num_2_float)
+    else:
+        print("Nunca deveria chegar aqui.")
+
     sair = input("Quer sair? [s]im ").lower().startswith('s')
 
     if sair is True:
         break
+    
